7dd7e835632c642b6a652ae3aae32bb7df90ed87
nikita23421 <nikitasimankovas12@gmail.com>
2023-04-27 14:08:28 +0300

    dar

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,11 +1,31 @@
-export default function RegisterPage (){
-    return(
-        <form className="register">
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/> 
-        <button>Register</button>               
-    </form>
-  
+import { useState } from "react";
 
-    );
+export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  async function register(ev) {
+    ev.preventDefault();
+    await fetch("http://localhost:4000/register", {
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  return (
+    <form className="register" onSubmit={register}>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+
+      <button>Register</button>
+    </form>
+  );
 }
